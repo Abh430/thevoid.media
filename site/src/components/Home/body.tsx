5adc4fcead29186bd9cbb62cb341bae24c6cf1a0
Andrew Hunter <abh430@gmail.com>
2024-09-19 02:26:03 -0400

    Add modal element to homepage with query for work items - moves modal state to top level page component with working modal component - sets up the work page to work properly when queried from the work landing page or homepage

--- site/src/components/Home/body.tsx
+++ site/src/components/Home/body.tsx
@@ -7,14 +7,19 @@ import ContactPage from "../contact/contactPage"
 
 import ScrollSpy from "react-ui-scrollspy";
 
+//data
+import { getWorkPageData } from "../work/workData";
 
-const BodyBlock = () => {
 
+const BodyBlock = (props) => {
+  const { openModal } = props;
+  const workItems = getWorkPageData();
+  
   return (
     <main className="max-w-10/12 2-10/12 px-4 sm:px-6 lg:px-0 gap-10">
       <ScrollSpy offsetTop={0} offsetBottom={40}>
         <AboutPage />
-        <WorkPage />
+        <WorkPage  workItems={workItems} hasModal={true} openModal={openModal} />
         <ContactPage />
       </ScrollSpy>
     </main>
--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,32 +1,82 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 
 // Components
 import Header from "../components/Home/header"
 import Body from "../components/Home/body"
 import Seo from "../components/seo"
+import { createPortal } from 'react-dom';
 
 import ScrollSpy from "react-ui-scrollspy";
 
-const IndexPage = () => (
+import WorkSingleton from "../components/work/workSingleton"
 
-  <div className="min-h-screen w-screen">
-    <Layout hasFooter={false} hasNav={false}>
-        <div className="max-w-7xl mx-auto lg:px-6 md:px-3 relative box-border flex flex-row gap-7 justify-between">
-          <Header />
-          <Body />
+const IndexPage = (props) => {
+console.log(props);
+const [modalIsVisible, setModalIsVisible] = useState(false);
+const [activeModalItem, setActiveModalItem] = useState(null);
+console.log(activeModalItem);
+
+function openModal(workItem) {
+  setActiveModalItem(workItem);
+  setModalIsVisible(true);
+}
+
+function closeModal() {
+  setModalIsVisible(false);
+}
+
+useEffect(() => {
+  document.body.style.overflowY = modalIsVisible ? "hidden" : "auto";
+  document.body.style.position = modalIsVisible ? "fixed" : "relative";
+}, [modalIsVisible]);
+
+
+const Modal = ({ onClose, isVisible, workItem }) => {
+  return (
+    <>
+      <div className={"modal bg-black/75 " + (isVisible ? "visible active" : "invisible")}>
+      <div className="max-w-7xl mx-auto modal-inner relative py-12 px-12">
+          <span className="modal-close text-zinc-50 fixed top-12 right-20 cursor-pointer p-4 border-solid border" onClick={() => onClose()}>X</span>
+          <WorkSingleton workItem={workItem} />
         </div>
-    </Layout>
-  </div>
-);
+      </div>
+    </>
+  );
+}
+
+
+  return (
+    <>
+      <div className="min-h-screen w-screen">
+        <Layout hasFooter={false} hasNav={false}>
+            <div className="max-w-7xl mx-auto lg:px-6 md:px-3 relative box-border flex flex-row gap-7 justify-between">
+              <Header />
+              <Body {...props} openModal={openModal} />
+            </div>
+        </Layout>
+      </div>
+      {createPortal(
+        <Modal onClose={() => closeModal()} isVisible={modalIsVisible} workItem={activeModalItem} />,
+        document.getElementById("modal-root")
+      )}
+    </>
+  );
+}
 
 
 
-export const Head = () => (
-  <Seo
-    title="The Void - Visual Media and Experience Designer"
-    description="I am da voyyyd."
-  ></Seo>
-);
+export const Head = (props) => {
+
+  return (
+    <Seo
+      title="The Void - Visual Media and Experience Designer"
+      description="I am da voyyyd."
+    >
+      <body className="" />
+  
+    </Seo>
+  );
+}
 
 export default IndexPage
