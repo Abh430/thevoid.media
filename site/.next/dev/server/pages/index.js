d64a8d74b0ce6fd56886fdbba147c3b3d60ce431
Andrew Hunter <abh430@gmail.com>
2026-01-20 20:17:44 -0700

    Remove next build files

--- site/.next/dev/server/pages/index.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/index.js")
-R.c("server/chunks/ssr/[root-of-the-server]__10ee84f1._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__84f891be._.js")
-R.c("server/chunks/ssr/f5d47__pnpm_91996852._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__8b70c95b._.js")
-R.m("[project]/site/node_modules/.pnpm/next@16.1.4_@babel+core@7.28.6_react-dom@19.2.3_react@19.2.3__react@19.2.3/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/site/src/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/site/node_modules/.pnpm/next@16.1.4_@babel+core@7.28.6_react-dom@19.2.3_react@19.2.3__react@19.2.3/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/site/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/site/node_modules/.pnpm/next@16.1.4_@babel+core@7.28.6_react-dom@19.2.3_react@19.2.3__react@19.2.3/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/site/src/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/site/node_modules/.pnpm/next@16.1.4_@babel+core@7.28.6_react-dom@19.2.3_react@19.2.3__react@19.2.3/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/site/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
