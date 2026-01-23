be5485d782a081141cec79af787023d2589bc5a2
Andrew Hunter <abh430@gmail.com>
2026-01-22 22:11:57 -0700

    Add CTA's for merch and vj shop

--- site/src/components/Home/body.tsx
+++ site/src/components/Home/body.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Fade } from "react-awesome-reveal"
 
 // Components
 import AboutPage from "../about/aboutPage"
@@ -6,6 +7,41 @@ import WorkPage from "../work/workPage"
 import ContactPage from "../contact/contactPage"
 
 import ScrollSpy from "react-ui-scrollspy";
+
+const MerchCTA = () => {
+  return (
+    <div className="max-w-7xl mx-auto mt-10 mb-20 px-2 md:px-8">
+      <Fade>
+        <div className="grid grid-cols-2 gap-4">
+          <a 
+            href="https://merch.thevoid.media" 
+            target="_blank" 
+            rel="noopener"
+            className="group block overflow-hidden rounded-lg transition-transform duration-300 hover:scale-[1.02]"
+          >
+            <img 
+              src="/images/merch-banner.jpg" 
+              alt="Shop The Void Merch" 
+              className="w-full h-auto object-cover grayscale transition-all duration-300 group-hover:grayscale-0"
+            />
+          </a>
+          <a 
+            href="https://shop.thevoid.media" 
+            target="_blank" 
+            rel="noopener"
+            className="group block overflow-hidden rounded-lg transition-transform duration-300 hover:scale-[1.02]"
+          >
+            <img 
+              src="/images/shop-banner.jpg" 
+              alt="Shop The Void" 
+              className="w-full h-auto object-cover grayscale transition-all duration-300 group-hover:grayscale-0"
+            />
+          </a>
+        </div>
+      </Fade>
+    </div>
+  );
+};
 
 const BodyBlock = (props) => {
   const { openModal, headerRef, bodyRef } = props;
@@ -16,6 +52,7 @@ const BodyBlock = (props) => {
         <AboutPage />
         <WorkPage  workItems={props.workItems} hasModal={true} openModal={openModal} />
         <ContactPage />
+        <MerchCTA />
       {/* </ScrollSpy> */}
     </main>
   )
